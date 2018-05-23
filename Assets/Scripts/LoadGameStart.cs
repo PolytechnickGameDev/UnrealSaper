90db163185a45667d9152cc0775fd5c95febed3c
Nazarkon <nazarmontsibovych@gmail.com>
2018-05-23 22:25:00 +0300

    Menu 1.0

---
+++ Assets/Scripts/LoadGameStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LoadGameStart : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+    {
+        EditorSceneManager.LoadScene(sceneIndex);
+
+    }
+}
