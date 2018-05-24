9f7c1c28b5d4b5930baba04fd7470fb3ce0af404
Mr-Computer <sashacomputer399@gmail.com>
2018-05-24 03:31:56 +0300

    Ohyet

---
+++ Assets/Scripts/HightLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HightLevel : MonoBehaviour {
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("3");
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene("5");
+    }
+    public void Level10()
+    {
+        SceneManager.LoadScene("10");
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene("MainWindow");
+    }
+
+}
