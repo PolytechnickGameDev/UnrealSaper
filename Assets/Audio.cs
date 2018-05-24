4e9529f4203a1d6893d733e5d5f0b48a4dab730c
Nazarkon <nazarmontsibovych@gmail.com>
2018-05-24 19:47:13 +0300

    Final Version

---
+++ Assets/Audio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+    public Slider slider;
+    public Text valueCount;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+        valueCount.text = slider.value.ToString();
+        AudioListener.volume = slider.value / 200;
+    }
+}
